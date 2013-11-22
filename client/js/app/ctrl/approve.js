cca41bc77f2d88ecdaea431dc7af73dbf9f957f7
Justin Permann <justin_permann@hotmail.com>
2013-11-21 18:43:54 -0700

    updates

--- client/js/app/ctrl/approve.js
+++ client/js/app/ctrl/approve.js
@@ -1,6 +1,21 @@
-/* global angular */
+/* global angular, console*/
 
 angular.module('app').controller('approveCtrl',
-  function (){
+  function ($scope, nodeSaver){
     'use strict';
+
+    $scope.configuration = [];
+    nodeSaver.retrieveConfigs(function(res) {
+      if (!res) {
+        console.log('uh oh...something went wrong!');
+        return [];
+      }
+      $scope.configurations = res;
+    });
+
+
+    $scope.approveConfig = function(selected) {
+      console.log(selected);
+
+    };
 });
